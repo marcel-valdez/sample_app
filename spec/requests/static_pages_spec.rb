7202e5b2b0ff14a3c307a970740c5a26323fa5f8
Marcel Valdez <marcel.valdez@live.com>
2012-07-05 17:04:29 -0500

    Finished static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,46 +1,67 @@
 require 'spec_helper'
 
+# @param [String] page_url relative path to the page to visit
+def specify_one_title(page_url)
+  it "should have only one title" do
+    visit page_url
+    page.should have_selector('title', count: 1)
+  end
+end
+
+# @param [String] page_url relative path to the page to visit
+# @param [String] page_title the title of the page
+def specify_title(page_url, page_title)
+  it "should have the right title" do
+    visit page_url
+    page.should have_selector(
+                    'title',
+                    text: "Ruby on Rails Tutorial Sample App | #{page_title}")
+  end
+end
+
+# @param [String] html_h1_content text content of the h1 element
+# @param [String] page_url relative path to the page to visit
+def specify_h1_text(page_url, html_h1_content)
+  it "should have the content '#{html_h1_content}'" do
+    visit page_url
+    page.should have_selector('h1', text: html_h1_content)
+  end
+end
+
 describe "StaticPages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector 'h1', text: 'Sample App'
-    end
+    page_url = '/static_pages/home'
+    page_title = "Home"
+    page_h1_text = "Sample App"
 
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector(
-                      'title',
-                      text: "Ruby on Rails Tutorial Sample App | Home")
-    end
+    specify_h1_text(page_url, page_h1_text)
+
+    specify_title(page_url, page_title)
+
+    specify_one_title page_url
   end
 
   describe "Help Page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector 'h1', text: 'Help'
-    end
+    page_url = '/static_pages/help'
+    page_title = 'Help'
 
-    it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector(
-                      'title',
-                      text: "Ruby on Rails Tutorial Sample App | Help")
-    end
+    specify_h1_text(page_url, page_title)
+
+    specify_title(page_url, page_title)
+
+    specify_one_title(page_url)
   end
 
   describe "About Page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector 'h1', text: 'About Us'
-    end
+    page_url = '/static_pages/about'
+    page_title = 'About Us'
 
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector(
-                      'title',
-                      text: "Ruby on Rails Tutorial Sample App | About Us")
-    end
+    specify_h1_text(page_url, page_title)
+
+    specify_title(page_url, page_title)
+
+    specify_one_title(page_url)
   end
 
 end
+
